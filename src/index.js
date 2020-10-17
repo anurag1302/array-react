653f4f8c72d07d08a7efde95312fdbc33d6f51d8
Anurag <anurag3487@gmail.com>
2020-10-17 23:09:36 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+
+const books = [
+  {
+    img: "https://m.media-amazon.com/images/I/816HBXHJsaL._AC_UY218_.jpg",
+    title: "React.js Book: Learning React JavaScript Library From Scratch",
+    author: "Greg Sidelnikov",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/71vlPquePfL._AC_UL320_.jpg",
+    title: "Pro C# 7: With .NET and .NET Core",
+    author: "Andrew Troelsen",
+  },
+];
+
+function BookList() {
+  return (
+    <section className="bookList">
+      {books.map((book) => {
+        console.log(book);
+        const { img, title, author } = book;
+        return <Book img={img} title={title} author={author} />;
+      })}
+    </section>
+  );
+}
+
+const Book = ({ img, title, author }) => {
+  return (
+    <article className="book">
+      <img src={img} alt="book"></img>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
